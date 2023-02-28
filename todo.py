8767d45bfa4b79588808a1f32c8f45805f3c9fb8
BKSaint <113442572+BKSaint@users.noreply.github.com>
2023-02-28 10:57:03 -0500

    To-Do-List

--- todo.py
+++ todo.py
@@ -1,12 +1,13 @@
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
-
+todos = ["go to the store", "buy groceries", "eat lunch"]
 @app.route("/")
 def index():
-    return render_template("todo.html.jinja", 
-                            todos = ["go to the store", "buy groceries", "eat lunch"])
+    return render_template("todo.html.jinja")
+
 
 @app.route("/app")
 def add():
     new_todo = request.form['new_todo']
+    todos.append(new_todo)
