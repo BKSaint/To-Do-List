e4a8607b09e617c8d95e833b5bf4145adc0d90f3
BKSaint <113442572+BKSaint@users.noreply.github.com>
2023-03-16 09:38:31 -0400

    password

--- todo.py
+++ todo.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template, request, redirect
+from flask_httpauth import HTTPBasicAuth
+from werkzeug.security import generate_password_hash, check_password_hash
+
 import pymysql
 import pymysql.cursors
 
@@ -14,7 +17,22 @@ connection = pymysql.connect(
 
 
 app = Flask(__name__)
+auth = HTTPBasicAuth()
+
+users = {
+    "saint": generate_password_hash("saint05")
+}
+
+@auth.verify_password
+def verify_password(username, password):
+    if username in users and \
+        check_password_hash(users.get(username), password):
+        return username
+
+
 @app.route("/")
+@auth.login_required
+
 def index():
     cursor = connection.cursor()
     cursor.execute("SELECT * FROM `Todo_Database` ORDER BY `Completed` DESC")
